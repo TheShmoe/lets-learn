47ea51483d8e17704538ced6151630c33e2ebc36
Joe Koppin <koppinjo@gmail.com>
2021-12-11 00:51:36 -0500

    First commit - euler 1-4 complete

---
+++ csharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using csharp.project_euler;
+
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ProjectEulerProblems = new Problems();
+
+            while (Console.ReadLine() != "quit")
+            {
+                Console.WriteLine($"Project Euler Problem 1 Solution: {ProjectEulerProblems.Problem1()}");
+                Console.WriteLine($"Project Euler Problem 2 Solution: {ProjectEulerProblems.Problem2()}");
+                Console.WriteLine($"Project Euler Problem 3 Solution: {ProjectEulerProblems.Problem3()}");
+                Console.WriteLine($"Project Euler Problem 4 Solution: {ProjectEulerProblems.Problem4()}");
+            }
+        }
+    }
+}
