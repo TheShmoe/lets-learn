d4f90fecdf13854978b7250e225ee865131ed38f
Joe Koppin <koppinjo@gmail.com>
2022-01-21 00:01:14 -0500

    new hollow angular app...with tailwind

---
+++ angular/shmoeland/src/app/_shared/shared.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+@NgModule({
+  exports: [
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatTooltipModule
+  ],
+  providers: []
+})
+export class SharedModule { }
